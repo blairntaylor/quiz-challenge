d6d91a033230ffebb3a0237459f337da1ed4e07e
Blair Taylor <blairntaylor@gmail.com>
2021-01-26 21:24:59 -0700

    Updating file names and removing test files

--- script.js
+++ script.js
@@ -1,75 +1,87 @@
-//Questions, Choices and Answers
-var questions = [{
-    question: "1. Commonly used data types DO NOT include",
-    choices: ["boolean", "number", "string", "iPod"],
-    answer: "iPod"
-},{
-    question: "2. Arrays in Javascript can be used to store ________",
-    choices: ["socks", "values", "cheeses", "events"],
-    answer: "values"
-},{
-    question: "3. String values must be enclosed in within ______ when being assigned to variables",
-    choices: ["two slices", "earmuffs", "quotes", "brackets"],
-    answer: "quotes"
-},
-]
-
-// questions start at 0 index
-varcurrentQuestionsIndex = 0; 
-// timer starts at 30 seconds
-var secondsLeft = 30;
-
-// START FUNCTION will hide the main div and show the question div and start timer
-// GET QUESTION FUNCTION goes through arrays of the questions
-// GET QUESTION FUNCTION define current question, displays the question, makes all buttons answer
-// CHECK ANSWER FUNCTION checks if the answer is wrong and subtracts time, moves you to the next question index, checks if you are at the end of the array
-// END QUIZ FUNCTION clears the timer, displays score, hides question div, displays end div
-// SAVE SCORE FUNCTION triggered with save score button
-// trigger all functions at the end 
-
-
-
-
-function startQuiz() {
-}
-
-function getQuestion() {
-        var currentQuestion = questions[currentQuestionIndex];
-}
-
-function questionClick(){ //this.value is the button
-    if (this.value !== questions[currentQuestionIndex].answer){
-        time -- 15;
-
-        if time(time < 0{
-            time = 0;
-        })
-    }
-}
-
-currentQuestion.choices.
-
-choiceNode.setAttribute('value", choice); // compare the answer
-choiceNode.onclick = questionClick;
-
-
-//Timer
+// calling data from the HTML
+var displayQuestions = document.getElementById("#display-questions");
 var timerEl = document.querySelector(".timer");
-//counting down from 30 seconds
-var seconds = 30;
-function timeLeft (){
-    var timerInterval = setInterval(function() {
-        seconds--; //counting down
-        timerEl.textContent = "Time left " + seconds;    
-        
-        if(seconds === 0) { //stop timer
-            clearInterval(timerInterval);
-            sendMessage("Quiz Over");
-          }
-    
-        }, 3000);
-      }
+var highScore = document.getElementById("#high-score");
+var viewScore = document.getElementById("#score-results");
+var startButton = document.getElementById("start-button");
+var saveButton = document.getElementById("#save-button");
+var quizSection = document.querySelector(".quiz");
+var displayQuestions = document.getElementById("#display-questions");
+var answer1 = document.getElementById("#answer1");
+var answer2 = document.getElementById("#answer2");
+var answer3 = document.getElementById("#answer3");
+var answer4 = document.getElementById("#answer4");
+var gameOver = document.getElementById("#game-over");
+var scoreResults = document.getElementById("#score-results");
 
-      choiceEl.innerHTML = ""; //clear the previous choices
+var questionIndex = 0;
+var secondsLeft = 30;
+var correct = 0;
+var incorrect = 0;
 
-//Score Board
+//  array of questions
+questionSelect = [
+    {"question": "1. Commonly used data types DO NOT include", 
+    "choices": ["boolean", "number", "string", "iPod"],
+    "answer": "iPod"},
+    {"question": "2. Arrays in Javascript can be used to store ________", 
+    "choices": ["socks", "values", "cheeses", "events"],
+    "answer": "values"},
+    {"question": "3. String values must be enclosed in within ______ when being assigned to variables",
+    "choices": ["two slices", "earmuffs", "quotes", "brackets"],
+    "answer": "quotes"}
+];
+
+//clicking on answers and check if correct
+answer1.addEventListener("click", checkAnswer)
+answer2.addEventListener("click", checkAnswer)
+answer3.addEventListener("click", checkAnswer)
+answer4.addEventListener("click", checkAnswer)
+
+// save user info
+saveButton.addEventListener("click", saveUser)
+
+// View time left
+// function setTimer() {
+//     timerEl.textContent = secondsLeft;
+// }
+
+// click on start button to show questions
+startButton.addEventListener("click", startTime);
+
+//function start quiz
+
+// setting up the timer
+function startTime() {
+    quizSection.style.display="block";
+    timer = setInterval(function() {
+        secondsLeft--;
+        timerEl.textContent = secondsLeft;
+
+        if(secondsLeft === 0) {
+            clearInterval(timer);
+            return 0;
+        }
+    }, 1000);
+}
+
+startTime();
+
+// function displayQuestion
+//display question and choices
+//ref the DOM
+//loop thru the questions
+//display questions on DOM
+//loop thru the choices
+//display each choice on DOM
+
+// functionquestionClick
+//check correctanswer against user choice
+//if answer correct, show next question
+// displayQuestion()
+// if incorect, subtract time
+// time = time - 15
+
+// function e
+// when all questions answered, game over
+// when time reaches 0 game over
